22117401b2546a78ae4dd85f7fa7410c4e54f5d4
Traumerei <atakan.sent@gmail.com>
2024-04-27 00:37:18 +0200

    lol

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas")
+const root = document.getElementById("root")
+
+const a = document.createElement("div")
+const b = document.createTextNode("testing")
+a.appendChild(b)
+
+root.appendChild(a)
